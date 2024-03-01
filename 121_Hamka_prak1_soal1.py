cea7819d72e960a647da301f0b55722ba7e57d80
Unnomed <Unnamedxd78@gmail.com>
2024-03-01 14:25:08 +0700

    commie 3

--- 121_Hamka_prak1_soal1.py
+++ 121_Hamka_prak1_soal1.py
@@ -2,18 +2,20 @@ batas1 = int(input("Masukan batas bawah :"))
 batas2 = int(input("Masukan batas atas:"))
 total = 0
 
+print("Batas bawahnya adalah :", batas1)
+print("Batas atasnya adalah :", batas2)
+
 if (batas1 >= 0 and batas2 >= 0) :
 
         for i in range (batas1, batas2):
     
             if (i % 2 != 0) :
                 total += 1
+                print(i)
             else:
                 pass
 else:
     print("Batas 1 atau batas 2 tidak boleh dibawah 0")
 
 
-print("Batas bawahnya adalah :", batas1)
-print("Batas atasnya adalah :", batas2)
 print("Maka totalnya adalah :", total)
